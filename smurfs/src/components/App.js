ad08be1547a4021c18fd6cfed463c48e248dd44e
1devhall <devoughnh@gmail.com>
2020-03-20 13:23:02 -0400

    mvp no styles

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
+import  SmurfForm  from '../components/SmurfForm';
+import  Smurfs  from '../components/Smurfs';
+import AddSmurf from './AddSmurf';
+
 class App extends Component {
   render() {
     return (
@@ -8,6 +12,9 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfForm />
+        <Smurfs />
+        <AddSmurf />
       </div>
     );
   }
